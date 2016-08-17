f03686a0a13e8ba37464293f1348028de36c82a5
Emily Do <emilyhdo@mit.edu>
2016-08-17 13:52:53 -0400

    Added gulp-util and gulp-coffee

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util')
+var coffee = require('gulp-coffee');
+
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare: true}).on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts/'));
+
+});
