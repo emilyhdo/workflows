4e8b532f20eb8f93f4ca38bf917e49d568f6de9f
Emily Do <emilyhdo@mit.edu>
2016-08-17 14:18:12 -0400

    Added gulp-compass to compile Sass to CSS using compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util')
+var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+var compass = require('gulp-compass');
+
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = ['components/scripts/pixgrid.js', 
+				'components/scripts/rclick.js',
+				'components/scripts/tagline.js',
+				'components/scripts/template.js'];
+var scssSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -10,3 +18,21 @@ gulp.task('coffee', function(){
 		.pipe(gulp.dest('components/scripts/'));
 
 });
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('builds/development/js/'));
+
+});
+
+gulp.task('compass', function(){
+	gulp.src(scssSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}).on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/css'));
+});
+
